9277fb88369c35b2222ad75ed23ddaea29397253
Justin-dr <justin.aurakingdom@gmail.com>
2025-05-12 23:59:32 +0200

    Bump version to 0.1.1

--- MyPluginInfo.cs
+++ MyPluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "Xepos.REPO-Mutators";
         public const string NAME = "Mutators";
-        public const string PLUGIN_VERSION = "0.1.0";
+        public const string PLUGIN_VERSION = "0.1.1";
     }
 }
