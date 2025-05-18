2d8552bbe10f08e5170a5313d70304bfe355fd0c
Justin-dr <justin.aurakingdom@gmail.com>
2025-05-18 22:17:44 +0200

    Grant player 3 levels of grab range when Rusty Servos is active

--- Mutators/Patches/RustyServosPatch.cs
+++ Mutators/Patches/RustyServosPatch.cs
@@ -1,16 +1,23 @@
 ﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Mutators.Mutators.Patches
 {
     internal class RustyServosPatch
     {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(PhysGrabber))]
+        [HarmonyPatch(nameof(PhysGrabber.Start))]
+        static void PhysGrabberStartPostfix(PhysGrabber __instance)
+        {
+            if (__instance != PhysGrabber.instance) return;
+            __instance.grabRange += 3f;
+        }
+
+
         [HarmonyPrefix]
         [HarmonyPatch(typeof(PlayerController))]
         [HarmonyPatch(nameof(PlayerController.Start))]
-        static void PlayerControllerStart(PlayerController __instance)
+        static void PlayerControllerStartPostfix(PlayerController __instance)
         {
             __instance.JumpCooldown = float.MaxValue;
         }
