280f0aaa108d3f2580d0ac031fa1849783e27839
Justin-dr <justin.aurakingdom@gmail.com>
2025-06-26 15:19:29 +0200

    Add multi mutator

---
+++ Mutators/MultiMutator.cs
@@ -0,0 +1,70 @@
+﻿using Mutators.Settings;
+using Sirenix.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Mutators.Mutators
+{
+    public class MultiMutator : IMutator
+    {
+        private readonly IList<Type> _patches;
+        private readonly IList<Func<bool>> _conditions;
+        private readonly IList<IMutator> _subMutators;
+
+        public string Name => Settings.MutatorName;
+
+        public string Description => Settings.MutatorDescription;
+
+        public bool Active { get; private set; }
+
+        public bool HasSpecialAction { get; private set; }
+
+        public AbstractMutatorSettings Settings { get; private set; }
+
+        public IReadOnlyList<Type> Patches => new ReadOnlyCollection<Type>(_patches);
+
+        public IReadOnlyList<Func<bool>> Conditions => new ReadOnlyCollection<Func<bool>>(_conditions);
+
+        public MultiMutator(AbstractMutatorSettings settings, IList<IMutator> mutators, IList<Func<bool>> conditions = null!)
+        {
+            Settings = settings;
+            _subMutators = mutators;
+
+            HasSpecialAction = _subMutators.Any(mutator => mutator.HasSpecialAction);
+
+            _conditions = mutators.SelectMany(mut => mut.Conditions).ToList();
+            if (conditions != null)
+            {
+                _conditions.AddRange(conditions);
+            }
+
+            _patches = mutators.SelectMany(mut => mut.Patches).ToList();
+        }
+
+        public void Patch()
+        {
+            if (Active) return;
+
+            Active = true;
+
+            foreach (IMutator subMutator in _subMutators)
+            {
+                subMutator.Patch();
+            }
+        }
+
+        public void Unpatch()
+        {
+            if (!Active) return;
+
+            foreach (IMutator subMutator in _subMutators)
+            {
+                subMutator.Unpatch();
+            }
+
+            Active = false;
+        }
+    }
+}
