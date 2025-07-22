48296369cfa73b5d257703b5b4a57eb1268dd9f8
Justin-dr <justin.xepos.deruijter@gmail.com>
2025-07-22 22:57:20 +0200

    Add MultiMutator setup

---
+++ Mutators/IMultiMutator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Mutators.Mutators
+{
+    public interface IMultiMutator : IMutator
+    {
+        IReadOnlyList<IMutator> SubMutators { get; }
+    }
+}
